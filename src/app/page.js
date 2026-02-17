7002915dc31a8a16b22970357286e1b59bd55008
Pradh123 <vermapradhumn3@gmail.com>
2026-02-17 08:54:43 +0530

    pradhumn verma

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     Hello World
+    <Link href="/login" className="bg-blue-500 px-2 py-1 rounded">Login here</Link>
     </div>
   );
 }
